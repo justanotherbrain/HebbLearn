cc64b5ae769767c9817e68b50bdaf5cbe1340499
justanotherbrain <rabadi@justanotherbrain.com>
2015-08-06 15:16:59 -0400

    yet another fix

--- stl-demo.py
+++ stl-demo.py
@@ -11,7 +11,7 @@ f = h5py.File('/scratch/mad573/stl10/unlabeled.mat')
 
 u = f['X'][()]
 
-temp = np.reshape(u, (3,96,96,100000), order='F')
+temp = np.reshape(u, (3,96,96,100000))
 temp = np.swapaxes(temp,0,2)
 
 
